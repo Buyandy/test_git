6697129baf20e72ae4b4fe087231c539f4dc45db
Ayapai7 <152875659+Ayapai7@users.noreply.github.com>
2025-05-19 00:55:23 +0700

    puhui

--- node_2d.tres.gd
+++ node_2d.tres.gd
@@ -1,11 +1,19 @@
 extends Sprite2D
 
-
+var speed = 1000
 # Called when the node enters the scene tree for the first time.
+
+func get_input():
+	var input_direction = Input.get_vector('a', "d", "w", "s")
+	var velocity = input_direction*speed
+	return velocity
+	
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position += get_input()*delta
+	
+	
